ad8bc2a786e9785003c4c7904dda864baa687291
Eric Ewe <eric@dsaid.gov.sg>
2021-04-08 01:45:46 +0800

    feat(*): add routes

---
+++ src/config/routes.js
@@ -0,0 +1,14 @@
+const routes = {
+  // Main Routes
+  HOME: "Home",
+  REPOSITORY: "Repository",
+  SIGN_IN: "SignIn",
+  SIGN_UP: "SignUp",
+  ISSUE_CREATE: "IssueCreate",
+
+  // Sub Routes
+  SEARCH_REPOSITORIES: "SearchRepositories",
+  BOOKMARKED_REPOSITORIES: "BookmarkedRepositories",
+};
+
+export default routes;
