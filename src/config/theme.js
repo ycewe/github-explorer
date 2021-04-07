e13153d5523b49d4d1f144655a7ae102b7982200
Eric Ewe <eric@dsaid.gov.sg>
2021-04-08 01:46:17 +0800

    feat(*): add colors as theme

---
+++ src/config/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    appBackground: "#E6E6FA",
+    appPrimary: "#14213D",
+    appSecondary: "#ADD8E6",
+    brandPrimary: "#524359",
+    brandSecondary: "#8D6A9F",
+
+    error: "#C33C54",
+    errorLight: "#FF9EB060",
+
+    success: "#228B22",
+    successLight: "#DCEAB2",
+
+    defaultDark: "#000000",
+    defaultLight: "#ffffff",
+  },
+};
+
+export default theme;
